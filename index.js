ba5f833d186ae516de999548e26088381a7b3a7a
Oscar Mesh <okerekeifeanyi65@gmail.com>
2022-07-01 17:12:21 +0100

    js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var state = {
-  balance: 0,
+  balance: 20000,
   income: 30000,
   expenses: 10000,
   transactions: [
@@ -8,15 +8,65 @@ var state = {
   ],
 };
 
-
 var balanceEl = document.querySelector("#balance");
 var incomeEl = document.querySelector("#income");
-var expenseEl = document.querySelector("#expenses")
-
+var expenseEl = document.querySelector("#expenses");
+var transactionsEl = document.querySelector("#transactions");
 
 function init() {
-    balanceEl.innerHTML = `#${state.balance}`
-    incomeEl.innerHTML =`#${state.income}`
-    expenseEl.innerHTML =`#${state.expenses}`
+  updateState();
+  render();
 }
-init ()
+
+function updateState() {
+  var balance = 0,
+    income = 0,
+    expense = 0,
+  item;
+
+  for (var i = 0; i < state.transactions.length; i++) {
+    item = state.transactions[i];
+
+    if (item.type === "income") {
+      income += item.amount;
+    } else if (item.type === "expense") {
+      expense += item.amount;
+    }
+  }
+
+  console.log(balance, income, expense);
+}
+
+function render() {
+  balanceEl.innerHTML = `#${state.balance}`;
+  incomeEl.innerHTML = `#${state.income}`;
+  expenseEl.innerHTML = `#${state.expenses}`;
+
+  var transactionEl, containerEl, amountEL, btnEl, item;
+
+  for (var i = 0; i < state.transactions.length; i++) {
+    item = state.transactions[i];
+    transactionEl = document.createElement("li");
+
+    transactionEl.append(item.name);
+    transactionsEl.appendChild(transactionEl);
+
+    containerEl = document.createElement("div");
+    amountEL = document.createElement("span");
+
+    if (item.type === "income") {
+      amountEL.classList.add("income-amt");
+    } else if (item.type === "expense") {
+      amountEL.classList.add("expense-amt");
+    }
+
+    amountEL.innerHTML = `#${item.amount}`;
+    containerEl.appendChild(amountEL);
+    btnEl = document.createElement("img");
+    btnEl.src = "cancel.svg";
+
+    containerEl.appendChild(btnEl);
+    transactionEl.appendChild(containerEl);
+  }
+}
+init();
